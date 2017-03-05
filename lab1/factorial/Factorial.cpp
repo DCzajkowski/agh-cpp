51978503b69034e07bc8f5f438360ec105fa3a4b
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-03-05 19:50:29 +0100

    Factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -1,8 +1,20 @@
 //
 // Created by mwypych on 02.02.17.
 //
+#include <cmath>
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+    if (std::fabs(value) > 12) {
+        return 0;
+    }
+
+    if (value == 0 || value == 1) {
+        return 1;
+    }
+
+    if (value > 0) {
+        return value * factorial(value - 1);
+    }
+    return value * factorial(value + 1);
 }
