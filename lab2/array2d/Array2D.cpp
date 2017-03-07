2f5c945b1cf940083255a41b27ac71cdf70a4216
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-03-07 12:10:42 +0100

    Array 2d

--- lab2/array2d/Array2D.cpp
+++ lab2/array2d/Array2D.cpp
@@ -2,18 +2,26 @@
 // Created by Dariusz Czajkowski on 07/03/17.
 //
 
+#include <iostream>
 #include "Array2D.h"
 
-int ** Array2D(int n_rows, int n_columns) {
+using namespace std;
+
+int ** NewArray2D(int n_rows, int n_columns) {
+    if (n_rows < 1 || n_columns < 1) {
+        return nullptr;
+    }
+
     int ** array = new int*[n_rows];
 
     for (int i = 0; i < n_rows; i++) {
         array[i] = new int[n_columns];
     }
 
+    int n = 1;
     for (int i = 0; i < n_rows; i++) {
         for (int j = 0; j < n_columns; j++) {
-            array[i][j] = i + j + 1;
+            array[i][j] = n++;
         }
     }
 
@@ -21,5 +29,17 @@ int ** Array2D(int n_rows, int n_columns) {
 }
 
 void DeleteArray2D(int ** array, int n_rows, int n_columns) {
+    for (int i = 0; i < n_rows; i++) {
+        delete [] array[i];
+    }
+    delete [] array;
+}
 
+void FillArray2D(int k_rows, int k_cols, int *to_fill[]) {
+    int n = 1;
+    for (int i = 0; i < k_rows; i++) {
+        for (int j = 0; j < k_cols; j++) {
+            to_fill[i][j] = n++;
+        }
+    }
 }
--- lab2/array2d/main.cpp
+++ lab2/array2d/main.cpp
@@ -2,3 +2,10 @@
 // Created by Dariusz Czajkowski on 07/03/17.
 //
 
+#include <iostream>
+#include "Array2D.h"
+
+int main() {
+
+    return 0;
+}
